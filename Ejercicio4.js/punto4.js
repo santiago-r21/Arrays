56eeb34ce83ff63e1a2529336810a6d238847ae2
santiago-r21 <santiagorioscajamarca@gmail.com>
2025-10-29 14:58:49 -0500

    Convertir cadena a numeros

---
+++ Ejercicio4.js/punto4.js
@@ -0,0 +1,6 @@
+const prompt = require("prompt-sync")();
+
+let num1 = prompt("Ingrese el primer numero ");
+let num2 = prompt("Ingrese el segundo numero ");
+let resul = Number(num1)+Number(num2);
+ console.log("el resultado de la suma es: ", resul);
